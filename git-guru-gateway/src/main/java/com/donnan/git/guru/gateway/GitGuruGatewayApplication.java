9d9a4aeca2ea1ca2519d6cd3c2abbcd94f5e884b
Donnan <1179596418@qq.com>
2025-03-21 19:47:54 +0800

    gateway start

--- git-guru-gateway/src/main/java/com/donnan/git/guru/gateway/GitGuruGatewayApplication.java
+++ git-guru-gateway/src/main/java/com/donnan/git/guru/gateway/GitGuruGatewayApplication.java
@@ -3,14 +3,14 @@ package com.donnan.git.guru.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
+@SpringBootApplication(scanBasePackages = "com.donnan.git.guru.gateway")
 public class GitGuruGatewayApplication {
 
-    @SpringBootApplication(scanBasePackages = "com.donnan.git.guru.gateway")
-    public class NfTurboGatewayApplication {
-
-        public static void main(String[] args) {
-            SpringApplication.run(NfTurboGatewayApplication.class, args);
-        }
-
+    public static void main(String[] args) {
+        SpringApplication.run(GitGuruGatewayApplication.class, args);
     }
+
 }
+
